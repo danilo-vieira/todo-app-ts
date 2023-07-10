f241abc67728e019f1890f5dc5361b789621dab7
Danilo Vieira <danilo_strvieira@hotmail.com>
2023-07-10 02:40:36 -0300

    feat: creates textarea component

---
+++ src/components/form/textarea/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import styles from './styles.module.scss'
+
+type TextAreaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
+  icon: React.ComponentType;
+  hasError?: boolean;
+  name: string;
+}
+
+const textareaRef = React.createRef<HTMLTextAreaElement>()
+
+export default class TextArea extends React.Component<TextAreaProps> {
+  state = {
+    value: ''
+  }
+
+  componentDidUpdate() {
+    textareaRef.current!.style.height = '0px';
+    const scrollHeight = textareaRef.current!.scrollHeight;
+
+    textareaRef.current!.style.height = `${scrollHeight}px`;
+  }
+  
+  render() {
+    const { icon: Icon, hasError, ...rest } = this.props
+
+    return (
+      <div onClick={() => textareaRef.current?.focus()} className={`${styles.container} ${hasError ? styles.errored : ''}`}>
+        <Icon />
+        <textarea onChange={ e => this.setState({ value: e.target.value }) } ref={textareaRef} {...rest} rows={1} maxLength={250} />
+      </div>
+    )
+  }
+}
