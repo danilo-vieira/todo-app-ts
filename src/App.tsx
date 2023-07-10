ea11c3c34ccdb4a308cc800f5e937d6611f2e7bb
Danilo Vieira <danilo_strvieira@hotmail.com>
2023-07-10 11:27:56 -0300

    feat: implements filters, sorting and redux with App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react"
+import { AnyAction } from "redux";
+import { ThunkDispatch } from "redux-thunk";
+import { connect } from "react-redux";
 
 import { Pencil, FileText, Calendar } from '@phosphor-icons/react';
+import * as z from 'zod';
+
+import parseZodErrors from "./helpers/parseZodErrors";
+import { asyncCreateTodoAction, setShowingAction, setSortingAction } from "@/redux/actions/todos.actions";
+import { RootState } from "./redux/reducers/todos.reducer";
 
 import Header from "@/components/header"
-
 import Form from "@/components/form";
 import Input from "@/components/form/input"
 import TextArea from "@/components/form/textarea";
@@ -13,24 +20,83 @@ import RadioGroup from "@/components/radio-group";
 import RadioItem from "@/components/radio-group/radio-item";
 import TodoCard from "./components/todo-card";
 
-class App extends React.Component {
+const createTodoSchema = z.object({
+  title: z.string().min(3, { message: 'O título deve ter no mínimo 3 caracteres' }),
+  description: z.string().nonempty({ message: 'A descrição não pode ser vazia' }),
+  deadline: z.string().nonempty({ message: 'O prazo não pode ser vazio' }),
+});
+
+type AppProps = {
+  addTodo: (todo: Todo) => void;
+  todos: Todo[];
+  isAdding: boolean;
+  showing: 'all' | 'completed' | 'pending';
+  sorting: 'newest' | 'oldest';
+  setShowing: (showing: 'all' | 'completed' | 'pending') => void;
+  setSorting: (sorting: 'newest' | 'oldest') => void;
+}
+
+type CreateTodoFormData = z.infer<typeof createTodoSchema>;
+
+class App extends React.Component<AppProps> {
+  state = {
+    errors: {
+      title: '',
+      description: '',
+      deadline: '',
+    }
+  }
+
+  onFormSubmit = (formData: CreateTodoFormData) => {
+    const { addTodo } = this.props;
+    const validationResult = createTodoSchema.safeParse(formData);
+    
+    if (!validationResult.success) {
+        return this.setState({
+        errors: {
+          title: parseZodErrors(validationResult, 'title'),
+          description: parseZodErrors(validationResult, 'description'),
+          deadline: parseZodErrors(validationResult, 'deadline'),
+        }
+      })
+    }
+
+    addTodo({
+      ...formData,
+      id: Date.now().toString(),
+      completed: false,
+      createdAt: new Date().toISOString(),
+    })
+
+    this.setState({
+      errors: {
+        title: '',
+        description: '',
+        deadline: '',
+      }
+    })
+  }
+
   render() {
+    const { todos, isAdding, sorting, showing, setShowing, setSorting } = this.props;
+
     return (
       <>
         <Header />
 
-        <Form onFormSubmit={(formData) => console.log(formData)}>
+        <Form onFormSubmit={this.onFormSubmit}>
           <Input
             name="title"
             placeholder="Título da tarefa" 
             icon={Pencil}
+            hasError={!!this.state.errors.title}
           />
 
           <TextArea
             name="description"
             placeholder="Descrição da tarefa" 
             icon={FileText}
-            defaultValue='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+            hasError={!!this.state.errors.description}
           />
 
           <Input
@@ -38,15 +104,19 @@ class App extends React.Component {
             name="deadline"
             placeholder="Prazo" 
             icon={Calendar}
+            hasError={!!this.state.errors.deadline}
           />
 
-          <Button type="submit">Add</Button>
+          <Button disabled={isAdding} type="submit">{isAdding ? 'Carregando...' : 'Add'}</Button>
         </Form>
 
         <div style={{ display: 'flex', gap: '1.6rem', marginTop: '3.2rem' }}>
           <RadioGroup
             title="Exibir:"
             defaultValue="all"
+            onValueChange={value => {
+              setShowing(value as 'all' | 'completed' | 'pending');
+            }}
           >
             <RadioItem name="Todos" value="all" />
             <RadioItem name="Somente concluídos" value="completed" />
@@ -56,6 +126,9 @@ class App extends React.Component {
           <RadioGroup
             title="Ordenação:"
             defaultValue="newest"
+            onValueChange={value => {
+              setSorting(value as 'newest' | 'oldest');
+            }}
           >
             <RadioItem name="Mais recentes primeiro" value="newest" />
             <RadioItem name="Mais antigas primeiro" value="oldest" />
@@ -71,64 +144,55 @@ class App extends React.Component {
             gridTemplateColumns: 'repeat(3, 1fr)',
           }}
         >
-          <li>
-            <TodoCard 
-              title="Título da tarefa"
-              description="Descrição da tarefa é um pouco longa, entao"
-              deadline="2023-07-10"
-              completed={false}
-              id="1"
-            />
-          </li>
-          <li>
-            <TodoCard 
-              title="Título da tarefa"
-              description="Descrição da tarefa é um pouco longa, entao"
-              deadline="2023-07-16"
-              completed={false}
-              id="2"
-            />
-          </li>
-          <li>
-            <TodoCard 
-              title="Título da tarefa"
-              description="Descrição da tarefa é um pouco longa, entao"
-              deadline="2023-07-11"
-              completed={false}
-              id="3"
-            />
-          </li>
-          <li>
-            <TodoCard 
-              title="Título da tarefa"
-              description="Descrição da tarefa é um pouco longa, entao"
-              deadline="2023-07-11"
-              completed={false}
-              id="4"
-            />
-          </li>
-          <li>
-            <TodoCard 
-              title="Título da tarefa"
-              description="Descrição da tarefa é um pouco longa, entao"
-              deadline="2023-07-11"
-              completed={false}
-              id="5"
-            />
-          </li>
-          <li>
-            <TodoCard 
-              title="Título da tarefa"
-              description="Descrição da tarefa é um pouco longa, entao"
-              deadline="2023-07-09"
-              completed={false}
-              id="6"
-            />
-          </li>
+          {todos
+          .sort((a, b) => {
+            if (sorting === 'newest') {
+              return new Date(a.createdAt) > new Date(b.createdAt) ? -1 : 1;
+            }
+
+            return new Date(a.createdAt) < new Date(b.createdAt) ? -1 : 1;
+          })
+          .filter(todo => {
+            if (showing === 'all') {
+              return true;
+            }
+
+            if (showing === 'completed') {
+              return todo.completed;
+            }
+
+            return !todo.completed;
+          })
+          .map(todo => (
+            <li key={todo.id}>
+              <TodoCard 
+                title={todo.title}
+                description={todo.description}
+                deadline={todo.deadline}
+                completed={todo.completed}
+                id={todo.id}
+              />
+            </li>
+          ))}
         </ul>
       </>
     )
   }
 }
 
-export default App
+const mapStateToProps = (state: RootState) => ({
+  todos: state.todos,
+  isAdding: state.isAdding,
+  showing: state.showing,
+  sorting: state.sorting,
+});
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<RootState, unknown, AnyAction>) => ({
+  addTodo: (todo: Todo) => dispatch(asyncCreateTodoAction(todo)),
+  setShowing: (showing: 'all' | 'completed' | 'pending') => dispatch(setShowingAction(showing)),
+  setSorting: (sorting: 'newest' | 'oldest') => dispatch(setSortingAction(sorting)),
+});
+
+const WithRedux = connect(mapStateToProps, mapDispatchToProps)(App);
+
+export default WithRedux;
